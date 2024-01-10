decb0f9cc730fc401db345a92197641acdea557f
User <huseynovali612@gmail.com>
2024-01-11 00:42:20 +0400

    create loin page

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+function LoginPage({ setUser }) {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Please enter a valid email address !")
+        .required("Email !!"),
+    }),
+    onSubmit: async (values) => {
+      let data = await axios.post("http://localhost:3000/user/login", values);
+
+      setUser(data);
+    },
+  });
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+
+          {formik.touched.email && formik.errors.email ? (
+            <div style={{ color: "red" }}>{formik.errors.email}</div>
+          ) : null}
+        </div>
+
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
--- server/service/socketIo.js
+++ server/service/socketIo.js
@@ -12,23 +12,23 @@ function initializeSocket(httpServer) {
   io.on("connection", (socket) => {
     console.log("user connect 1");
     socket.on("newUser", async (UserName) => {
-      console.log("user connect");
-      let user = await User.findOne({ name: UserName });
-      if (user) {
-        user.SocketId = socket.id;
-        user.status = true;
-        user.save();
-      }
+     
+    //  let user = await User.findOne({ name: UserName });
+    //   if (user) {
+    //     user.SocketId = socket.id;
+    //     user.status = true;
+    //     user.save();
+    //   }
     });
 
     socket.on("disconnect", async () => {
       console.log("user disconnect");
-      let user = await User.findOne({ SocketId: socket.id });
-      if (user) {
-        user.SocketId = "";
-        user.status = false;
-        user.save();
-      }
+    //   let user = await User.findOne({ SocketId: socket.id });
+    //   if (user) {
+    //     user.SocketId = "";
+    //     user.status = false;
+    //     user.save();
+    //   }
     });
   });
 }
